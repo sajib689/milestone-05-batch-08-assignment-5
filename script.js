358def7ac5c9294939f43d28ac3475510592f78a
sajib689 <jibonraja11@gmail.com>
2024-02-15 15:26:15 +0600

    get the total amoun

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const cards = document.querySelectorAll('.card')
 let counters = 1;
-
+let sum = 0;
 // card loop
 
 for (var index = 0; index < cards.length; index++) {
@@ -16,6 +16,10 @@ for (var index = 0; index < cards.length; index++) {
         // end product counter and products get name processed completely
 
         // get the product total amount
+        const totalAmountElement = document.getElementById('totalPrice')
+        const totalPrice = parseFloat(card.querySelector('h3').innerText.split(" ")[1])
+        totalAmountElement.innerText =  sum += totalPrice
+        
         
     })
 }
