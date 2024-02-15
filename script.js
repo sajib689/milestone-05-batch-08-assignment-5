9e0c308f72cbf3f65f1f2cef834e9f4b50955271
sajib689 <jibonraja11@gmail.com>
2024-02-15 13:17:50 +0600

    get the name

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const cards = document.querySelectorAll('.card')
-
+let counters = 1;
 
 // card loop
 
 for (var index = 0; index < cards.length; index++) {
     const card = cards[index]
     card.addEventListener('click', function () {
-        const title = card.querySelector('h2')
-        console.log(title.innerText)
+        // get the products name
+        const title = card.querySelector('h2').innerText
+        const titleList = document.getElementById('title-container')
+        const p = document.createElement('p')
+        p.innerText =counters +'.'+ title
+        titleList.appendChild(p)
+        counters++
+        // end product counter and products get name processed completely
+
+        // get the product total amount
+        
     })
 }
